2d6952a65c9e001f305e1c70f00bf8b6942d2004
p0o9i8u7y6t5r4e3w2q1 <rose8783@gmail.com>
2019-07-23 23:08:20 +0800

    add test for model

--- test/dbtest.ts
+++ test/dbtest.ts
@@ -5,14 +5,16 @@ import * as Model from '../src/model/entity';
 createConnection()
   .then(async connection => {
     console.log('connection create success');
-    // 1. User -- success
+    // 1. User
     // const repository = connection.getRepository(Model.User);
-    // const user = await repository.find();
-    // console.log(user);
-    //
-    // 2. Teacher
-    const repository = connection.getRepository(Model.Teacher);
-    const teacher = await repository.findOne({ name: '王惠嘉' });
-    console.log(teacher);
+    const user = await connection.manager.findOne(Model.User, {
+      email: 'rsliu@mail.ncku.edu.tw',
+    });
+    console.log(user);
+
+    // 2. Teacher -- failure
+    // const repository = connection.getRepository(Model.Teacher);
+    // const teacher = await repository.findOne({ where: { name: '王惠嘉' } });
+    // console.log(teacher);
   })
   .catch(error => console.log(error));
